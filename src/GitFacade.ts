654f2818434472efefb88883af6e99b866a84f05
Teo Gebhard <teo@gebhard.fi>
2025-01-05 01:35:35 +0200

    remove unnecessary escaping

--- src/GitFacade.ts
+++ src/GitFacade.ts
@@ -60,7 +60,7 @@ export class GitFacade {
     }
 
     async getLatestFixedCommit(): Promise<Commit> {
-        return (await this.queryCommits('--grep-reflog=rebase \(fixup\)', '--walk-reflogs', '-1'))[0]
+        return (await this.queryCommits('--grep-reflog=rebase (fixup)', '--walk-reflogs', '-1'))[0]
     }
 
     private async queryCommits(...args: string[]): Promise<Commit[]> {
